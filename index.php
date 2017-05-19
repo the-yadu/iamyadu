1bb56724829ec353ad454172da405554999bd857
NarayaN Yaduvanshi <narayan@ykone.com>
2017-05-19 23:25:32 +0530

    moved navigation

--- index.php
+++ index.php
@@ -33,133 +33,107 @@
 
 <body>
     <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="/">Home</a>
-            </div>
+    <?php
+        require('./src/nav.php');
+    ?>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="http://dailyblogvlog.com">blog</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Header -->
-    <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="site-heading">
-                        <h1>NarayaN Yaduvanshi</h1>
-                        <hr class="small">
-                        <span class="subheading">Also known as NarayaN Singh in official documents</span>
+        <!-- Page Header -->
+        <!-- Set your background image for this header on the line below. -->
+        <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <div class="site-heading">
+                            <h1>NarayaN Yaduvanshi</h1>
+                            <hr class="small">
+                            <span class="subheading">Also known as NarayaN Singh in official documents</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-    <!-- Main Content -->
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="post-preview">
-
-                    <h2 class="post-title">
-                        Skills
-                    </h2>
-                    <div>
-                        <label>Server Side : </label> NodeJS, ExpressJs, VueJs
-                    </div>
-                    <div>
-                        <label>AI & ML : </label> Machine learning, API.ai, Wit.ai
-                    </div>
-                    <div>
-                        <label>Databases & frameworks : </label> Mysql, Mongo, Mongoose, SQL, Cassandra<sup>*</sup>
-                    </div>
-                    <div>
-                        <label>Frontend : </label> React, Angular, Vue<sup>*</sup>
-                    </div>
-                    <div>
-                        <label>Mobile : </label> React Native, <a href="http://nativebase.io">Native Base</a>,Ionic
-                    </div>
-                </div>
-                <hr>
-            </div>
-        </div>
-    </div>
-
-    <hr>
-
-    <!-- Footer -->
-    <footer>
+        <!-- Main Content -->
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <ul class="list-inline text-center">
-                        <li>
-                            <a href="http://www.twitter.com/the_yadu">
-                                <span class="fa-stack fa-lg">
+                    <div class="post-preview">
+
+                        <h2 class="post-title">
+                            Skills
+                        </h2>
+                        <div>
+                            <label>Server Side : </label> NodeJS, ExpressJs, VueJs
+                        </div>
+                        <div>
+                            <label>AI & ML : </label> Machine learning, API.ai, Wit.ai
+                        </div>
+                        <div>
+                            <label>Databases & frameworks : </label> Mysql, Mongo, Mongoose, SQL, Cassandra<sup>*</sup>
+                        </div>
+                        <div>
+                            <label>Frontend : </label> React, Angular, Vue<sup>*</sup>
+                        </div>
+                        <div>
+                            <label>Mobile : </label> React Native, <a href="http://nativebase.io">Native Base</a>,Ionic
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <ul class="list-inline text-center">
+                            <li>
+                                <a href="http://www.twitter.com/the_yadu">
+                                    <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
                                 </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://www.facebook.com/imyadu">
-                                <span class="fa-stack fa-lg">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.facebook.com/imyadu">
+                                    <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
                                 </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-github fa-stack-1x fa-inverse"></i>
                                 </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <p class="copyright text-muted">Copyright &copy; iamyadu.com 2017</p>
+                                </a>
+                            </li>
+                        </ul>
+                        <p class="copyright text-muted">Copyright &copy; iamyadu.com 2017</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
 
-    <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+        <!-- jQuery -->
+        <script src="vendor/jquery/jquery.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
+        <!-- Contact Form JavaScript -->
+        <script src="js/jqBootstrapValidation.js"></script>
+        <script src="js/contact_me.js"></script>
 
-    <!-- Theme JavaScript -->
-    <script src="js/clean-blog.min.js"></script>
+        <!-- Theme JavaScript -->
+        <script src="js/clean-blog.min.js"></script>
 
 </body>
 
